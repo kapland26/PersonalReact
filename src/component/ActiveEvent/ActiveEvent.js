80a8d695d2cd2e8cae365921fb69d6949c9eee1b
unknown <kapland26backup@gmail.com>
2018-05-13 21:38:06 -0600

    Particle js added to active event leave page

--- src/component/ActiveEvent/ActiveEvent.js
+++ src/component/ActiveEvent/ActiveEvent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux'; 
 import io from 'socket.io-client';
+import Particles from 'react-particles-js';
 import './ActiveEvent.css';
 import peace from './peace-temp.png';
 import wait from './wait.png';
@@ -198,10 +199,14 @@ class ActiveEvent extends Component {
                                 <div className= "guest-list" >
                                     <p>Event: {event.name} </p>
                                     <p>User: {user.name} </p>
-                                    <p>Event id: {event.event_id}</p>
-                                    <p>People left in Room: {event.users_remaining}</p>
                                     <p>People Invited: {event.users_invited}</p>
-                                    <p>EOP limit: {event.end_user_amount}</p>
+
+
+                                    {/* <p>Event id: {event.event_id}</p>
+                                    <p>People left in Room: {event.users_remaining}</p>
+                                    <p>EOP limit: {event.end_user_amount}</p> */}
+
+                                    <br/>
                                     <div classnmae="timerContainer">
                                         Timer to Leave: 
                                         <FormControl className={classes.formControl}>
@@ -233,7 +238,72 @@ class ActiveEvent extends Component {
                         </div>
                     ):
                     <div className="leave">
-                            BYE
+                     <Particles className="particleJs"
+                        params={{
+                            particles: {
+                                number: { value: 80, density: { enable: true, value_area: 800 } },
+                                color: { value:  ["#EF5350","#4DB6AC","#EEFF41","#000000"]},
+                                shape: {
+                                    type: "circle",
+                                    stroke: { width: 0, color: "#000000" },
+                                    polygon: { nb_sides: 5 },
+                                    image: { src: "img/github.svg", width: 100, height: 100 }
+                                },
+                                opacity: {
+                                    value: 0.9,
+                                    random: false,
+                                    anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
+                                },
+                                size: {
+                                    value: 3,
+                                    random: true,
+                                    anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
+                                },
+                                line_linked: {
+                                    enable: false,
+                                    distance: 150,
+                                    color: "#ffffff",
+                                    opacity: 0.4,
+                                    width: 1
+                                },
+                                move: {
+                                    enable: true,
+                                    speed: 6,
+                                    direction: "none",
+                                    random: false,
+                                    straight:false,
+                                    out_mode: "out",
+                                    bounce: false,
+                                    attract: { enable: false, rotateX: 600, rotateY: 1200 }
+                                }
+                            },
+                            interactivity: {
+                                detect_on: "canvas",
+                                events: {
+                                onhover: { enable: true, mode: "repulse" },
+                                onclick: { enable: true, mode: "push" },
+                                resize: true
+                                },
+                                modes: {
+                                grab: { distance: 400, line_linked: { opacity: 1 } },
+                                bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
+                                repulse: { distance: 100, duration: 0.4 },
+                                push: { particles_nb: 4 },
+                                remove: { particles_nb: 2 }
+                                }
+                            },
+                            retina_detect: true
+                            }}                
+                        style={{
+                            top: '50px',
+                            left: '0px',
+                            position: 'fixed',
+                            width: '100vw',
+                            height: '100vh',
+                            zIndex: 0
+                        }}
+                    />
+                     <p>  LEAVE </p>
                     </div>
                 }
                 </div>
